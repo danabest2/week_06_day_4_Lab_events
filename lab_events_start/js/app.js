5a74048156edddf6ade073f246b993c1c7f48b67
DANIELA <danielamiritescu@gmail.com>
2019-08-01 15:36:33 +0100

    updated reading list

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
-})
+
+const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = event.target.title.value;
+    const author = event.target.author.value;
+    const category = event.target.category.value;
+    const formResult = document.querySelector('#reading-list');
+    formResult.textContent = `${title} ${author} ${category}`;
+  });
+
+
+});
